232ea0ad424cd606eb52783ae636b9e571e8a562
Aryan Singh <113651343+Aryan1423@users.noreply.github.com>
2024-08-08 02:15:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyC-bG9YiAsfRVe_-WyYN7oo3kUkp83_u9g",
+    authDomain: "kunall-2480b.firebaseapp.com",
+    databaseURL: "https://kunall-2480b-default-rtdb.firebaseio.com",
+    projectId: "kunall-2480b",
+    storageBucket: "kunall-2480b.appspot.com",
+    messagingSenderId: "699766755166",
+    appId: "1:699766755166:android:dcb77938d3328ccf9d09cf"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
